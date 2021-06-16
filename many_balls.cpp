0361581a680084e59186a96e1b5f88bb5e95f045
s-e-a-70 <82727720+s-e-a-70@users.noreply.github.com>
2021-06-16 17:19:52 +0300

    Два шарика с вект и усреднением, struct и return
    Два шарика, усреднение скорости первого по шаблону длины 5, доступ к структурам через скобки и звездочки, функция диагностики столкновения с возвращаемым значением

--- many_balls.cpp
+++ many_balls.cpp
@@ -1,12 +1,25 @@
 #include "TxLib.h"
 #include <math.h>
 
-void Balls_Move();
-void Draw_Ball    (int x, int y, COLORREF border_color, COLORREF fill_color);
-void Draw_Vector  (int x, int y, int vx, int vy, COLORREF border_color);
-void Average_Velosity (int vx, int vy, int vectorVx[5], int vectorVy[5], int *average_count, int *vx_sr, int *vy_sr);
-void Physics_Ball (int *x, int *y, int *vx, int *vy, int ax, int ay, int dt);
-void Control_Ball (int *vx, int *vy);
+struct balls
+{
+    int x;
+    int y;
+    int vx;
+    int vy;
+    int r;
+    COLORREF border_color;
+    COLORREF fill_color;
+
+} ;
+
+void Balls_Move ();
+void Draw_Ball        (balls  ball);
+void Draw_Vector      (balls  ball);
+void Average_Velosity (balls *ball, int vectorVx[5], int vectorVy[5], int *average_count, int *vx_sr, int *vy_sr);
+void Physics_Ball     (balls *ball, int ax, int ay, int dt);
+void Control_Ball     (balls *ball);
+bool Is_Crash         (balls  ball1, balls ball2, int eps);
 
 //-----------------------------------------------------
 int main()
@@ -24,55 +37,68 @@ int main()
 void Balls_Move()
 {
 
-    int x1  = 100;
-    int y1  = 0;
-    int vx1 = 5;
-    int vy1 = 3;
+    struct balls ball1;
+    struct balls ball2;
+
+    ball1.x  = 100;
+    ball1.y  = 0;
+    ball1.vx = 5;
+    ball1.vy = 3;
+    ball1.r  = 20;
+
+    ball1.border_color = TX_MAGENTA;
+    ball1.fill_color   = TX_LIGHTMAGENTA;
+
     int ax1 = 1;
     int ay1 = 0;
 
-    int x2  = 0;
-    int y2  = 100;
-    int vx2 = 2;
-    int vy2 = 1;
+    ball2.x  = 0;
+    ball2.y  = 100;
+    ball2.vx = 2;
+    ball2.vy = 1;
+    ball2.r  = 20;
+
+    ball2.border_color = TX_BLUE;
+    ball2.fill_color   = TX_LIGHTBLUE;
+
     int ax2 = 0;
-    int ay2 = 0;
+    int ay2 = 1;
 
     int dt = 1;
     int count = 0;
-    int l = 0;
-    int lmin = 1000;
 
-    int vectorVx[5] = {0, 0, 0, 0, 0};
-    int vectorVy[5] = {0, 0, 0, 0, 0};
+
+
+    int vectorVx[5]    = {0, 0, 0, 0, 0};
+    int vectorVy[5]    = {0, 0, 0, 0, 0};
     int average_count  = 0;
     int vx_sr          = 0;
     int vy_sr          = 0;
 
-    vx_sr = vx1;
-    vy_sr = vy1;
+    vx_sr = ball1.vx;
+    vy_sr = ball1.vy;
+
+
 
     while (!txGetAsyncKeyState (VK_ESCAPE))
     {
 
-        Draw_Ball        (x1, y1, TX_MAGENTA, TX_LIGHTMAGENTA);
-        Draw_Vector      (x1, y1, vx_sr, vx_sr, TX_MAGENTA);
-        Physics_Ball     (&x1, &y1, &vx1, &vy1, ax1, ay1, dt);
-        Control_Ball     (&vx1,&vy1);
-        Average_Velosity (vx1, vy1, vectorVx, vectorVy, &average_count, &vx_sr, &vy_sr);
+        Draw_Ball        (ball1);
+        Draw_Vector      (ball1);
+        Physics_Ball     (&ball1, ax1, ay1, dt);
+        Control_Ball     (&ball1);
+        Average_Velosity (&ball1, vectorVx, vectorVy, &average_count, &vx_sr, &vy_sr);
 
-        Draw_Ball    (x2, y2, TX_BLUE, TX_LIGHTBLUE);
-        Draw_Vector  (x2, y2, vx2, vy2, TX_MAGENTA);
-        Physics_Ball (&x2, &y2, &vx2, &vy2, ax2, ay2, dt);
-        Control_Ball (&vx2, &vy2);
+        Draw_Ball    (ball2);
+        Draw_Vector  (ball2);
+        Physics_Ball (&ball2, ax2, ay2, dt);
+        Control_Ball (&ball2);
 
-        if ((x1 == x2) && (y1 == y2))
-            count++;
+        if (Is_Crash(ball1, ball2, 40))
+            count ++;
 
-        l = (int)sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+        printf("CRASH = %d\r", count);
 
-        if ( l < lmin )
-            lmin = l;
 
 
         txSleep (100);
@@ -83,73 +109,73 @@ void Balls_Move()
 }
 
 //-----------------------------------------------------
-void Draw_Ball(int x, int y, COLORREF border_color, COLORREF fill_color)
+void Draw_Ball(balls ball)
 {
 
-    txSetColor     (border_color);
-    txSetFillColor (fill_color);
+    txSetColor     (ball.border_color);
+    txSetFillColor (ball.fill_color);
 
-    txCircle    (x, y, 20);
+    txCircle    (ball.x, ball.y, ball.r);
 
 }
-
-void Physics_Ball(int *x, int *y, int *vx, int *vy, int ax, int ay, int dt)
+//-----------------------------------------------------
+void Physics_Ball(balls *ball, int ax, int ay, int dt)
 {
 
-    *vx += ax*dt;
-    *vy += ay*dt;
+    (*ball).vx += ax*dt;
+    (*ball).vy += ay*dt;
 
-    *x+= *vx*dt;
-    *y+= *vy*dt;
+    (*ball).x += (*ball).vx*dt;
+    (*ball).y += (*ball).vy*dt;
 
-     if (*x > 800)
+     if ((*ball).x > 800)
     {
-       *vx = -(*vx);
-       *x  = 800;
+       (*ball).vx = -((*ball).vx);
+      (*ball).x  = 800;
     }
 
-    if (*y > 600)
+    if ((*ball).y > 600)
     {
-       *vy = -(*vy);
-       *y  = 600;
+       (*ball).vy = -((*ball).vy);
+       (*ball).y  = 600;
     }
 
-    if (*x < 0)
+    if ((*ball).x < 0)
     {
-       *vx = -(*vx);
-       *x  = 0;
+       (*ball).vx = -((*ball).vx);
+       (*ball).x  = 0;
     }
 
-    if (*y < 0)
+    if ((*ball).y < 0)
     {
-       *vy = -(*vy);
-       *y  = 0;
+       (*ball).vy = -((*ball).vy);
+       (*ball).y  = 0;
     }
 
 }
-
-void Control_Ball(int *vx, int *vy)
+//-----------------------------------------------------
+void Control_Ball(balls *ball)
 {
 
-    if(txGetAsyncKeyState (VK_RIGHT)) (*vx)++;
-    if(txGetAsyncKeyState (VK_LEFT))  (*vx)--;
-    if(txGetAsyncKeyState (VK_UP))    (*vy)++;
-    if(txGetAsyncKeyState (VK_DOWN))  (*vy)--;
-    if(txGetAsyncKeyState (VK_SPACE)) {*vx = 0; *vy = 0;}
+    if(txGetAsyncKeyState (VK_RIGHT)) ((*ball).vx)++;
+    if(txGetAsyncKeyState (VK_LEFT))  ((*ball).vx)--;
+    if(txGetAsyncKeyState (VK_UP))    ((*ball).vy)++;
+    if(txGetAsyncKeyState (VK_DOWN))  ((*ball).vy)--;
+    if(txGetAsyncKeyState (VK_SPACE)) {(*ball).vx = 0; (*ball).vy = 0;}
 
 }
-
-void Draw_Vector(int x, int y, int vx, int vy, COLORREF border_color)
+//-----------------------------------------------------
+void Draw_Vector(balls ball)
 {
 
-    txSetColor     (border_color);
+    txSetColor     (ball.border_color);
 
-    txLine   (x, y, x + 3 * vx, y + 3 * vy);
-    txCircle (x + 3 * vx, y + 3 * vy, 3);
+    txLine   (ball.x, ball.y, ball.x + 3 * ball.vx, ball.y + 3 * ball.vy);
+    txCircle (ball.x + 3 * ball.vx, ball.y + 3 * ball.vy, 3);
 
 }
-
-void Average_Velosity(int vx, int vy, int vectorVx[5], int vectorVy[5], int *average_count, int *vx_sr, int *vy_sr)
+//-----------------------------------------------------
+void Average_Velosity(balls *ball, int vectorVx[5], int vectorVy[5], int *average_count, int *vx_sr, int *vy_sr)
 {
 
     int svx = 0;
@@ -157,10 +183,10 @@ void Average_Velosity(int vx, int vy, int vectorVx[5], int vectorVy[5], int *ave
     int i   = 0;
 
 
-    vectorVx[*average_count] = vx;
-    vectorVy[*average_count] = vy;
+    vectorVx[*average_count] = (*ball).vx;
+    vectorVy[*average_count] = (*ball).vy;
 
-    if (*average_count == 5)
+    if (*average_count == 4)
     {
         svx = 0;
         svy = 0;
@@ -183,3 +209,19 @@ void Average_Velosity(int vx, int vy, int vectorVx[5], int vectorVy[5], int *ave
     (*average_count)++;
 
 }
+//-----------------------------------------------------
+bool Is_Crash (balls ball1, balls ball2, int eps)
+{
+
+    bool crash = false;
+    int  l     = 0;
+
+    l = (int)sqrt((ball1.x- ball2.x) * (ball1.x - ball2.x) + (ball1.y - ball2.y) * (ball1.y - ball2.y));
+
+    if (l <= eps)
+        crash = true;
+
+    return crash;
+
+}
+//-----------------------------------------------------
